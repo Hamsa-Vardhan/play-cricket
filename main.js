17dcd8fae8f3f7e79ec9c49e54ee4bb953bf92eb
Hamsa-Vardhan <hamsavardhan99@gmail.com>
2020-05-15 20:32:36 +0530

    added files

---
+++ main.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function (_) {
+    document.body.innerHTML = 'Hello there ha bsjkflzxbn vjskda,mnsvafjdJSKJ NCK';
+});
+//# sourceMappingURL=main.js.map
